d7172272131cec42042bf552d7457cbbd59b6866
mtenyg24 <m.gatluak@alustudent.com>
2023-12-24 23:37:52 +0300

    react

---
+++ mylab/src/providers/MealsProvider.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const MealsContext=React.createContext();
+
+const todaysMeals=["Baked Beans", "Baked Sweet Potatoes", "Baked Potatoes"];
+
+const MealsProvider=( {children} ) => {
+    const meals = React.useState( todaysMeals );
+
+    return (
+        <MealsContext.Provider value= {{meals}}></MealsContext.Provider>
+    )
+}
+
+export const useMealsListContext=() => React.useContext( MealsContext );
+
+export default MealsProvider 
