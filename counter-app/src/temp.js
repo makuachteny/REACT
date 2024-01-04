96ba5724fcd48a343e097e092ec60562af3384bf
mtenyg24 <m.gatluak@alustudent.com>
2024-01-04 20:48:05 +0300

    color update()

--- counter-app/src/temp.js
+++ counter-app/src/temp.js
@@ -1,20 +1,38 @@
 import React, { useState } from "react";
 function TempChange(props) {
-  const [temp, setTemp] = useState(20);
-  return (
-    <div className="app-container">
-      <div className="temp-display-container">
-        <div className="temp">{temp}°C</div>
-        <button className="temp-btn" onClick={() => setTemp(temp + 1)}>
-          +
-        </button>
-        <button className="temp-btn" onClick={() => setTemp(temp - 1)}>
-          -
-        </button>
-          </div>
-          <button className="reset-btn" onClick={() => setTemp(0)}>Reset</button>
-    </div>
-  );
+        const [temp, setTemp]=useState( 10 );
+    const [tempColor, setTempColor]=useState( "cold" );
+    
+    const increaseTemp=() => {
+        const newTemp=temp+1;
+        if (newTemp >= 15) {
+            setTempColor("hot")
+        }
+        setTemp(newTemp)
+    }
+
+    const decreaseTemp=() => {
+        const newTemp=temp-1;
+        if (newTemp <= 14) {
+            setTempColor("cold")
+        }
+        setTemp(newTemp)
+    }
+        
+    return (
+        <div className="app-container">
+            <div className="temp-display-container">
+                <div className={`temp ${tempColor}`}>{temp}°C</div>
+                <button className="temp-btn" onClick={() => increaseTemp()}>
+                    +
+                </button>
+                <button className="temp-btn" onClick={() => decreaseTemp()}>
+                    -
+                </button>
+            </div>
+            <button className="reset-btn" onClick={() => setTemp(10)}>Reset</button>
+        </div>
+    );
 }
 
 export default TempChange;
